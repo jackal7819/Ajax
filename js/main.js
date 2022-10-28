431687032f16880882e4912fb0703c715081bd15
Viktor <106143167+jackal7819@users.noreply.github.com>
2022-10-28 21:38:36 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,29 @@
-let url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
 let newValue = document.createElement('h4');
+let url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
 let output = 0;
-let rateCurrency = 0;
 let input = document.querySelector('#input');
-let inputValue = Number(input.value);
+let inputValue = 0;
 let btnCurrency = document.querySelectorAll('.exchange__button');
+let rateCurrency = 0;
+let exchange = document.querySelector('.exchange');
+exchange.append(newValue);
+newValue.innerHTML = `You can buy: ${output}`;
 
-fetch(url)
-    .then((response) => response.json())
-    .then((currency) => {
-        currency.forEach((item) => {
-            amountCurrency(item);
-        });
-    });
-
-function amountCurrency(item) {
-    for (let elem of btnCurrency) {
-        elem.addEventListener('click', () => {
-            if (elem.getAttribute('data-rate') === item.cc) {
-                exchangeRate(elem);
-            }
-        });
-
-        function exchangeRate(elem) {
-            rateCurrency = item.rate;
-            output = Math.floor(inputValue / rateCurrency);
-            newValue.replaceWith(newValue);
-            newValue.innerHTML = `You can buy: ${output} ${elem.textContent}`;
-        }
-
-        let exchange = document.querySelector('.exchange');
-        exchange.append(newValue);
-        newValue.innerHTML = `You can buy: ${output}`;
+btnCurrency.forEach((elem) => {
+    elem.addEventListener('click', amountCurrency);
+    function amountCurrency() {
+        fetch(url)
+            .then((response) => response.json())
+            .then((currency) => {
+                currency.forEach((item) => {
+                    if (item.cc === elem.textContent) {
+                        rateCurrency = item.rate;
+                    }
+                    inputValue = Number(input.value);
+                    output = Math.floor(inputValue / rateCurrency);
+                    newValue.replaceWith(newValue);
+                    newValue.innerHTML = `You can buy: ${output} ${elem.textContent}`;
+                });
+            });
     }
-}
+});
